1cc114593d657cf0492c33346ce0746f4c50cad9
lawal <sphade010@gmail.com>
2025-03-30 20:11:15 +0100

    fix: update auth client base URL to use environment variable

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,8 +1,9 @@
+import { PUBLIC_API_BASE_URL } from '$env/static/public';
 import type { TUser } from '@repo/server/types';
 import { inferAdditionalFields, organizationClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/svelte';
 export const authClient = createAuthClient({
-	baseURL: 'http://127.0.0.1:8787/', // the base url of your auth server,
+	baseURL: PUBLIC_API_BASE_URL, // the base url of your auth server,
 	plugins: [
 		organizationClient(),
 		inferAdditionalFields({
