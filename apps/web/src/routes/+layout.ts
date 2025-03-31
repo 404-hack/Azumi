e481730e25ea5089d304bccb171e1a6bca17ee15
lawal <sphade010@gmail.com>
2025-03-31 19:26:53 +0100

    fix: add debug log for session data in load function

--- apps/web/src/routes/+layout.ts
+++ apps/web/src/routes/+layout.ts
@@ -4,6 +4,7 @@ import { authClient } from '$lib/auth-client';
 
 export const load = async () => {
 	const session = await authClient.getSession();
+	console.log('🚀 ~ load ~ session:', session);
 
 	return {
 		user: session.data?.user
